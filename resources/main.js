414e1588bced8562debc615746327b04c9776055
Sven <sven@nimiq.com>
2018-10-24 21:48:37 +0800

    feature bomb: load fotomoto and font async, make crypto copy to clipboard on click, show only landscape/portrait in header slider, close fullscreen at end; toggle dark/bright theme; animation tweaks

--- resources/main.js
+++ resources/main.js
@@ -2,10 +2,12 @@
 let fotomotoLoaded = false,
     fotomotoCallback = [];
 function load() {
-    let local = location.href.indexOf('file:///') > -1,
-        container = document.querySelector("section.stories"),
-        storyTemplate = getTemplate(".story", container),
-        imageTemplate = getTemplate("img", storyTemplate);
+    let local           = location.href.indexOf('file:///') > -1,
+        container       = document.querySelector("section.stories"),
+        storyTemplate   = getTemplate(".story", container),
+        imageTemplate   = getTemplate("img", storyTemplate),
+        portraitImages  = [],
+        landscapeImages = [];
 
     function getTemplate(selector, container) {
         let template = document.querySelector(selector + ".template", container);
@@ -39,15 +41,21 @@ function load() {
                         FOTOMOTO.API.checkinImage(imageUrl(story, position));
                     }
                 }});
-        var img       = slide.img,
-            container = slide.container,
-            buy       = container.querySelector(".buy"),
-            checkout  = container.querySelector(".checkout");
+        const img       = slide.img,
+              container = slide.container,
+              buy       = container.querySelector(".buy"),
+              checkout  = container.querySelector(".checkout");
+              close     = container.querySelector(".close");
 
         function nextHandler(e) {
             e.stopPropagation();
             if (e.detail > 1) return; // double or more clicks
-            slide.next();
+            if (slide.position < story.files.length - 1) {
+                slide.next();
+            }
+            else {
+                close.click();
+            }
         }
         function buyHandler(e) {
             e.stopPropagation();
@@ -68,7 +76,7 @@ function load() {
             img[1][fn]('click', nextHandler, false);
             img[1][fn]('touchstart', nextHandler, false);
             img[1][fn]('dragstart', nextHandler, false);
-            container.querySelector(".close")[fn]('click', closeHandler, false);
+            close[fn]('click', closeHandler, false);
             buy[fn]('click', buyHandler, false);
             checkout[fn]('click', checkoutHandler, false);
         }
@@ -104,6 +112,8 @@ function load() {
                     }
                     loadImages(story, index + 1, container, priority);
                 }, 1 + index*10 + priority*50);
+
+                ((image.width > image.height) ? landscapeImages : portraitImages).push(imageUrl(story, index));
             }
         }
         let story = template.cloneNode(true);
@@ -117,12 +127,16 @@ function load() {
         return story;
     }
 
-    function randomIndex(a) { return Math.floor(Math.random() * a.length); }
-
     function slideShow(data) {
-        var slide = new window.slide({
+        function randomIndex(a) { return Math.floor(Math.random() * a.length); }
+
+        let images = window.innerWidth > window.innerHeight ? landscapeImages : portraitImages,
+            slide = new window.slide({
                 id: 'header .welcome',
                 source: function imageSource(position) {
+                    if (images.length > 0) {
+                        return images[randomIndex(images)];
+                    }
                     const story = data[randomIndex(data)];
                     return imageUrl(story, randomIndex(story.files));
                 },
@@ -130,12 +144,61 @@ function load() {
                 auto: 10000 });
     }
 
+    // hooks
+    document.querySelectorAll('.intro code').forEach(function(code) {
+        code.addEventListener('click', function(e) {
+            const address = code.innerText;
+            let range      = document.createRange(),
+                successful = false;
+            range.selectNode(code);
+            window.getSelection().removeAllRanges();
+            window.getSelection().addRange(range);
+
+            if (navigator.clipboard) {
+                navigator.clipboard.writeText(address).then(function() { successful = true; }, function(e) { });
+            }
+            else {
+                try {
+                    successful = document.execCommand('copy');
+                } catch (err) { }
+            }
+            if (successful) {
+                window.getSelection().removeAllRanges();
+                code.innerText = 'copied!';
+                setTimeout(function() { code.innerText = address; }, 1000);
+            }
+        });
+        code.title = 'Click to copy address';
+    });
+    document.querySelector('.theme.toggle').addEventListener('click', function(e){
+        const htmlClasses = document.documentElement.classList,
+              isBright = htmlClasses.contains('bright');
+        htmlClasses.toggle('bright', !isBright);
+        htmlClasses.toggle('dark', isBright);
+    });
+
+    // init
+
     slideShow(data);
 
     let priority = 0
     for (let storyData of data) {
         add(renderStory(storyTemplate, storyData, priority++), container);
     }
+
+    // load fotomoto
+    function loadScript(url, callback) {
+        var script = document.createElement("script");
+        if (callback) {
+            script.onload = callback;
+        }
+        document.head.appendChild(script);
+        script.src = url;
+    }
+
+    loadScript("https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.slim.min.js", function() {
+        loadScript("//widget.fotomoto.com/stores/script/7ac4f5e25748acfee580b24ed7c401c3003b6fe5.js?api=true");
+    });
 }
 
 function fotomoto_loaded() {
